3df09b2af32beeb76bcad99615e1dc0a760c5c77
Rinat273 <bairam0487@gmail.com>
2023-09-29 19:37:18 +0400

    Lesson 3 and 4

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,51 @@
 class QuestionsController < ApplicationController
+  def show
+    @question = Question.find_by id: params[:id]
+  end
+
+  def destroy
+    @question = Question.find_by id: params[:id]
+    @question.destroy
+    flash[:success] = "Question deleted!"
+    redirect_to questions_path
+  end
+
+  def edit
+    @question = Question.find_by id: params[:id]
+  end
+  
+  def update
+    @question = Question.find_by id: params[:id]
+    if @question.update questions_params
+      flash[:success] = "Question updated!"
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
   def index
     @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new questions_params
+    if @question.save
+      flash[:success] = "Question created!"
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def questions_params
+    params.require(:question).permit(:title, :body)
   end
 
 end
